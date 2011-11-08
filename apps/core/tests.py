66f1c0b9d23a3f344a182b4b5bc33c2f2b99583d
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-08 12:06:14 -0400

    Initial test commit (non working)

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,11 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+
+from core.runtime import gpg
+from content import Resource, ContentCache
 
 
 class SimpleTest(TestCase):
@@ -14,3 +10,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+'''
+class GPGTest(TestCase):
+    verify = gpg.verify_file('/tmp/gpgtest/testfile.txt.gpg')
+    gpg.sign_file('/tmp/gpgtest/testfile.txt', '/tmp/gpgtest/new')
+
+    res = Resource('/tmp/gpgtest/image001.png')
+    print res.uuid
+    #print res.data
+
+    cache = ContentCache('/tmp')
+    cache.store(res)
+'''
