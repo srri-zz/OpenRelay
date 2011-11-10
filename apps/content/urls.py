66fb3868168528b95519f4ee9e0c7560fa8ff688
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-10 10:13:12 -0400

    Added resource serving view and passing of user specified key and resource name to the resource upload view

--- apps/content/urls.py
+++ apps/content/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('content.views',
-    url(r'^serve/(?P<resource_id>[\d\w]+)/$', 'serve_resource', (), 'serve_resource'),
+    url(r'^serve/(?P<uuid>.*)/$', 'resource_serve', (), 'resource_serve'),
     url(r'^upload/$', 'resource_upload', (), 'resource_upload'),
     url(r'^list/$', 'resource_list', (), 'resource_list'),
-    #url(r'^update/(?P<resource_id>[\d\w]+)/$', 'upload_resource', (), 'update_resource'),
 )
