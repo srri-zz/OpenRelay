e328c763ea565ebe769cdbb668a627714730739b
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-11 02:30:25 -0400

    Added proejct version code and set it to an initial value 0.1

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
+from core import __version__
+
 urlpatterns = patterns('core.views',
     url(r'^$', direct_to_template, {'template': 'home.html'}, 'home_view'),
-    url(r'^about/$', direct_to_template, {'template': 'about.html'}, 'about_view'),
+    url(r'^about/$', direct_to_template, {'template': 'about.html', 'extra_context': {'version': __version__}}, 'about_view'),
     #url(r'^changelog/$', 'changelog_view', (), 'changelog_view'),
     #url(r'^license/$', 'license_view', (), 'license_view'),
 )
