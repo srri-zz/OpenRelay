e84064e007c1b56b915625fd257c5c80d92021c6
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-14 05:56:30 -0400

    Added passphrase fields and validation to the key creation form

--- apps/django_gpg/forms.py
+++ apps/django_gpg/forms.py
@@ -1,24 +1,37 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-#from django_gpg import GPG, Key
-
 
 class NewKeyForm(forms.Form):
     name = forms.CharField(
         label=_(u'Real name'),
-        #help_text=_(u'A name that uniquely identifies this resource, if left blank the filename is used instead.'),
-        #required=Truee,
+        help_text=_(u'Your real name.'),
     )
 
     comment = forms.CharField(
         label=_(u'Comment'),
-        widget=forms.widgets.Textarea()
-        #help_text=_(u'The private key that will be used to sign the file.'),
+        widget=forms.widgets.Textarea(attrs={'rows': 4}),
+        help_text=_(u'A comment or a note to help identify this key.'),
     )
     
     email = forms.CharField(
         label=_('Email'), 
-        #help_text=_(u'Automatically convert relative references to images, links, CSS and Javascript.'),
-        #initial=True
     )
+
+    passphrase = forms.CharField(
+        label=_(u'Passphrase'),
+        widget=forms.widgets.PasswordInput(),
+        required=False,
+    )
+
+    passphrase_verify = forms.CharField(
+        label=_(u'Passphrase (verification)'),
+        widget=forms.widgets.PasswordInput(),
+        required=False,
+    )
+
+    def clean(self):
+        if self.cleaned_data['passphrase'] != self.cleaned_data['passphrase_verify']:
+            raise forms.ValidationError(_(u'Both passphrase fields entries must match.'))
+
+        return self.cleaned_data
