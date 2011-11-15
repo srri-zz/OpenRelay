7c15542dedb57dfa1014a0d914e0fef7d382e26d
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-15 19:14:29 -0400

    Initial commit of the announce service

--- apps/server_talk/views.py
+++ apps/server_talk/views.py
@@ -1,9 +1,16 @@
 """The root view for OpenRelay API"""
 
+from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 
 from djangorestframework.mixins import InstanceMixin, ReadModelMixin
 from djangorestframework.views import View, ModelView
+
+from server_talk.models import LocalNode
 
 
 class OpenRelayAPI(View):
@@ -17,9 +24,29 @@ class OpenRelayAPI(View):
     """
 
     def get(self, request):
-        return [{'name': 'Resources', 'url': reverse('resource-root')}]
+        return [
+            {'name': 'Resources', 'url': reverse('resource-root')},
+            {'name': 'Services', 'url': reverse('service-root')}
+        ]
 
 
 class ReadOnlyInstanceModelView(InstanceMixin, ReadModelMixin, ModelView):
     """A view which provides default operations for read only against a model instance."""
     _suffix = 'Instance'
+
+
+
+class Services(View):
+    def get(self, request):
+        return [{'name': 'Announce', 'url': reverse('service-announce')}]
+
+
+class Announce(View):
+    def post(self, request):
+        return {'uuid': LocalNode.get().uuid}
+
+
+#def join(request):
+    #request.POST
+    
+#def inventory_view(request):
