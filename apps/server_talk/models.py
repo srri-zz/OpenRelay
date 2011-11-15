41635158d4aef805be4f8e17a097369d1c348bba
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-15 19:14:44 -0400

    Added models to hold the local node information as well as its inventory of sibling nodes

--- apps/server_talk/models.py
+++ apps/server_talk/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from openrelay_resources.models import ResourceBase
+
+
+class Nodebase(models.Model):
+    uuid = models.CharField(max_length=48, editable=False, verbose_name=_(u'UUID'))
+    
+    def __unicode__(self):
+        return self.uuid
+
+    class Meta:
+        abstract = True
+        verbose_name = _(u'node')
+        verbose_name_plural = _(u'nodes')
+        
+        
+class LocalNode(Nodebase):
+    lock_id = models.CharField(max_length=1, default='1', editable=False, verbose_name=_(u'lock field'), unique=True)
+
+    @classmethod
+    def get(cls):
+        return cls.objects.get(lock_id='1')
+
+    def save(self, *args, **kwargs):
+        self.id=1
+        super(LocalNode, self).save()
+
+    def delete(self):
+        pass
+
+    class Meta(Nodebase.Meta):
+        verbose_name = _(u'local node')
+        verbose_name_plural = _(u'local node')
+    
+
+class Sibling(Nodebase):
+    ip_address = models.IPAddressField(verbose_name=_(u'IP address'))
+
+
+class Resource(ResourceBase):
+    pass
+
+
+#class AnnounceQueue(models.Model):
+    #datetime = models.DateTimeField(verbose_name=_(u'datetime received'))
+    #node = 
+    
+    
+    
+    
+    
