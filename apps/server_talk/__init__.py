fe5f177a3bf64b72aa0c2db8976c44099b166bba
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-15 19:15:30 -0400

    Added identity initialization on database creation signal

---
+++ apps/server_talk/__init__.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db.models.signals import post_syncdb
+from django.core.signals import request_finished
+from django.dispatch import receiver
+
+from server_talk.models import LocalNode
+from server_talk import models as server_talk_model
+
+
+@receiver(post_syncdb, dispatch_uid='create_identify', sender=server_talk_model)
+def create_identify(sender, **kwargs):
+    LocalNode.objects.get_or_create(lock_id='1', defaults={'uuid': unicode(uuid.uuid4())})
