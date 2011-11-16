7a8eee57ce14528851ae0bcd3accffa15463369a
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-16 04:27:01 -0400

    Replace the django test Client with python requests, improved the announce api client call

--- apps/server_talk/api.py
+++ apps/server_talk/api.py
@@ -10,35 +10,59 @@
 # report: blacklist a server that has repeatedly tampered with files
 
 import urlparse
+import logging
+import socket
 
-from django.test.client import Client
+import requests
+
 from django.utils.simplejson import dumps, loads
 from django.core.urlresolvers import reverse
 
 from djangorestframework import status
 
-#HTTP_200_OK = 200
-#HTTP_201_CREATED = 201
-#HTTP_202_ACCEPTED = 202
-#HTTP_203_NON_AUTHORITATIVE_INFORMATION = 203
-#HTTP_204_NO_CONTENT = 204
-#HTTP_205_RESET_CONTENT = 205
-#HTTP_206_PARTIAL_CONTENT = 206
+from server_talk.models import LocalNode, Sibling
+from server_talk.conf.settings import PORT
+from server_talk.exceptions import AnnounceClientError
+
+logger = logging.getLogger(__name__)
+
 
 class RemoteCall(object):
-    
-    def __init__(self, url=None, uuid=None):
-        self.url=url
-        self.uuid=uuid
-    
+    def __init__(self, *args, **kwargs):
+        self.ip_address = kwargs.pop('ip_address', '')
+        self.port = kwargs .pop('port', '')
+        self.uuid = kwargs.pop('uuid', None)
     
     def announce(self):
         '''
         Announce ourselves to another OpenRelay node
         '''
-        c = Client()
-        response = c.post(urlparse.urljoin(self.url, reverse('service-announce')), {'name': 'fred', 'passwd': 'secret'})
-        if response.status_code == status.HTTP_200_OK:
-            print loads(response.content)
+        local_node_info = {
+            'ip_address': socket.gethostbyname(socket.gethostname()),
+            'port': PORT,
+            'uuid': LocalNode.get().uuid,
+        }
+        full_ip_address = u'%s%s' % (self.ip_address, u':%s' % self.port if self.port else '')
+        url = urlparse.urlunparse(['http', full_ip_address, reverse('service-announce'), '', '', ''])
+        
+        try:
+            response = requests.post(url, data=local_node_info)
+        except requests.ConnectionError:
+            logger.error('ERROR: unable to connect to url: %s' % url)
+            raise AnnounceClientError('Unable to join network')
+
+        if response.status_code == status.OK:
+            node_answer = loads(response.content)
+            if node_answer['uuid'] == LocalNode.get().uuid:
+                logger.error('ERROR: announce service on node with uuid: %s and url: %s, responded the same UUID as the local server' % (node_answer['uuid'], full_ip_address))
+                raise AnnounceClientError('Remote and local nodes identity conflict')
+            else:
+                sibling_data = {'ip_address':  node_answer['ip_address'], 'port': node_answer['port']}
+                sibling, created = Sibling.objects.get_or_create(uuid=node_answer['uuid'], defaults=sibling_data)
+                if not created:
+                    sibling.ip_address = sibling_data['ip_address']
+                    sibling.port = sibling_data['port']
+                    sibling.save()                
         else:
-            print 'error'
+            logger.error('ERROR: announce service on remote node responded with a non OK code')
+            raise AnnounceClientError('Unable to join network')
