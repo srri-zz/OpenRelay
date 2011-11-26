ed1f857d3e492a91aa01f8992f2c30f88b8d47e8
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-26 15:39:41 -0400

    Added missing prefixed slash / to url

--- apps/openrelay_resources/urls.py
+++ apps/openrelay_resources/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('openrelay_resources.views',
     url(r'^serve/(?P<uuid>.+)/$', 'resource_serve', (), 'resource_serve'),
     url(r'^upload/$', 'resource_upload', (), 'resource_upload'),
     url(r'^list/$', 'resource_list', (), 'resource_list'),
-    url(r'^details(?P<uuid>.+)/$', 'resource_details', (), 'resource_details'),
+    url(r'^details/(?P<uuid>.+)/$', 'resource_details', (), 'resource_details'),
 )
