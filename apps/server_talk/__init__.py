c8bfeb756579aa5a3e9e85063b88892423ab1174
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-28 08:51:15 -0400

    Create and automatically publish the node's public/private key pair when the database is first initialized

--- apps/server_talk/__init__.py
+++ apps/server_talk/__init__.py
@@ -1,26 +1,61 @@
 import logging
-import uuid
 
 from django.db.models.signals import post_syncdb
 from django.dispatch import receiver
 
 from scheduler import register_interval_job
+from core.runtime import gpg
 
 from server_talk import models as server_talk_model
 from server_talk.tasks import heartbeat_check, inventory_hash_check
 from server_talk.conf.settings import (HEARTBEAT_QUERY_INTERVAL,
     INVENTORY_QUERY_INTERVAL)
 from server_talk.models import LocalNode
+from server_talk.api import NetworkCall
 
 logger = logging.getLogger(__name__)
 
 
 @receiver(post_syncdb, dispatch_uid='create_identify', sender=server_talk_model)
 def create_identify(sender, **kwargs):
-    print 'Creating local node identity ...'
-    info, created = LocalNode.objects.get_or_create(lock_id='1', defaults={'uuid': unicode(uuid.uuid4())})
-    if not created:
-        print 'Existing identify not modified.'
+    node, created = LocalNode.objects.get_or_create(lock_id='1')
+    if created or not node.uuid:
+        print 'Creating local node identity ...'
+        name_real = raw_input('Enter a name to describe this node: ')
+        name_email = raw_input('Enter an e-mail associated with this node: ')
+        name_comment = raw_input('Enter comment for this node: ')
+        while True:
+            passphrase = raw_input('Enter a passphrase to lock this key: ')
+            passphrase_verify = raw_input('Enter again the same passphrase to verify: ')
+            if passphrase == passphrase_verify:
+                break
+            else:
+                print '\nPassphrases do not match, try again.\n'
+        key_args = {
+            'name_real': name_real,
+            'name_email': name_email,
+            'name_comment': name_comment,
+            'passphrase': passphrase,
+            'key_type': 'RSA',
+            'key_length': 2048,
+            'subkey_type': 'RSA',
+            'subkey_length': 2048,
+            'expire_date': 0,
+        }
+        print 'Creating node key, type on the keyboard, move the mouse, utilize the disks to generate entropy.'
+        try:
+            key = gpg.create_key(**key_args)
+            node.uuid = key.fingerprint
+            node.name = name_real
+            node.email = name_email
+            node.comment = name_comment
+            node.save()
+            network = NetworkCall()
+            network.publish_key(key)
+        except Exception, msg:
+            print 'Unhandled exception: %s' % msg
+    else:
+        print 'Existing identity not modified.'
 
 
 register_interval_job('heartbeat_check', heartbeat_check, seconds=HEARTBEAT_QUERY_INTERVAL)
