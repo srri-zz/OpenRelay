62864f71f6ee1c146a4fd7adca46ffcd936dced8
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-10 10:49:25 -0400

    Active sibling_hash_check scheduled task and add SERVER_SIBLINGS_QUERY_INTERVAL configuration option

--- apps/server_talk/conf/settings.py
+++ apps/server_talk/conf/settings.py
@@ -5,5 +5,6 @@ from django.conf import settings
 PORT = getattr(settings, 'SERVER_PORT', 8000)
 IPADDRESS = getattr(settings, 'SERVER_IPADDRESS', socket.gethostbyname(socket.gethostname()))
 HEARTBEAT_QUERY_INTERVAL = getattr(settings, 'SERVER_HEARTBEAT_QUERY_INTERVAL', 10)
-INVENTORY_QUERY_INTERVAL = getattr(settings, 'SERVER_INVENTORY_QUERY_INTERVAL', 11)
+INVENTORY_QUERY_INTERVAL = getattr(settings, 'SERVER_INVENTORY_QUERY_INTERVAL', 30)
+SIBLINGS_QUERY_INTERVAL = getattr(settings, 'SERVER_SIBLINGS_QUERY_INTERVAL', 30)
 HEARTBEAT_FAILURE_THRESHOLD = getattr(settings, 'SERVER_HEARTBEAT_FAILURE_THRESHOLD', 30)
