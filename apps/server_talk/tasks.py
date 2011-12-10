ff94619aa4de5a4872254d328ad5c196c7e20499
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-10 13:42:03 -0400

    Improve exception handling for the scheduled task with atomizing the exception detection scopes

--- apps/server_talk/tasks.py
+++ apps/server_talk/tasks.py
@@ -6,7 +6,8 @@ from django.utils.simplejson import dumps
 from lock_manager import Lock, LockError
 from openrelay_resources.literals import TIMESTAMP_SEPARATOR
 
-from server_talk.exceptions import HeartbeatError, InventoryHashError
+from server_talk.exceptions import (HeartbeatError, InventoryHashError,
+    SiblingsHashError, SiblingsListError, ResourceListError)
 from server_talk.api import RemoteCall
 from server_talk.models import LocalNode, Sibling, NetworkResourceVersion, ResourceHolder
 from server_talk.literals import NODE_STATUS_DOWN, NODE_STATUS_UP
@@ -25,24 +26,27 @@ def heartbeat_check():
         oldest = siblings[0]
         try:
             lock = Lock.acquire_lock(u''.join(['heartbeat_check', oldest.uuid]), 20)
-            node = RemoteCall(uuid=oldest.uuid)
-            oldest.last_heartbeat = datetime.datetime.now()
-            response = node.heartbeat()
-            oldest.cpuload = int(float(response['cpuload']))
-            oldest.status = NODE_STATUS_UP
-            oldest.failure_count = 0
-            oldest.save()
-            lock.release()
         except LockError:
-            pass
-        except HeartbeatError:
-            oldest.status = NODE_STATUS_DOWN
-            oldest.failure_count += 1
-            oldest.save()
-            if oldest.failure_count > HEARTBEAT_FAILURE_THRESHOLD:
-                oldest.delete()
-
-            lock.release()
+            logging.debug('unable to acquire lock')
+        else:
+            node = RemoteCall(uuid=oldest.uuid)
+            try:
+                response = node.heartbeat()
+            except HeartbeatError:
+                oldest.status = NODE_STATUS_DOWN
+                oldest.failure_count += 1
+                oldest.last_heartbeat = datetime.datetime.now()
+                oldest.save()
+                if oldest.failure_count > HEARTBEAT_FAILURE_THRESHOLD:
+                    oldest.delete()
+            else:
+                oldest.last_heartbeat = datetime.datetime.now()
+                oldest.cpuload = int(float(response['cpuload']))
+                oldest.status = NODE_STATUS_UP
+                oldest.failure_count = 0
+                oldest.save()
+            finally:
+                lock.release()
 
 
 # TODO: move DB logic to api.py
@@ -56,35 +60,43 @@ def inventory_hash_check():
         oldest = siblings[0]
         try:
             lock = Lock.acquire_lock(u''.join(['inventory_hash', oldest.uuid]), 20)
-            oldest.last_inventory_hash = datetime.datetime.now()
-            remote_api = RemoteCall(uuid=oldest.uuid)
-            response = remote_api.inventory_hash()
-            if oldest.inventory_hash != response['inventory_hash']:
-                # Delete this holder from all it's resources to catch
-                # later the ones it doesn't have anymore
-                ResourceHolder.objects.filter(node__uuid=oldest.uuid).delete()
-                for resource_item in remote_api.resource_list():
-                    uuid, timestamp=resource_item['uuid'].split(TIMESTAMP_SEPARATOR)
-                    resource, created = NetworkResourceVersion.objects.get_or_create(
-                        uuid=uuid,
-                        timestamp=timestamp,
-                        defaults={
-                            'metadata': dumps(resource_item.get('metadata')),
-                            'signature_properties': dumps(resource_item.get('signature_properties')),
-                        }
-                    )
-                    resource.resourceholder_set.get_or_create(node=oldest)
-                
-                oldest.inventory_hash = response['inventory_hash']
-                oldest.save()
-                # Delete network resources that have no holder
-                NetworkResourceVersion.objects.filter(resourceholder=None).delete()
-            lock.release()
         except LockError:
-            pass
-        except InventoryHashError:
-            lock.release()
+            logging.debug('unable to acquire lock')
+        else:        
+            remote_api = RemoteCall(uuid=oldest.uuid)
+            try:
+                response = remote_api.inventory_hash()
+            except InventoryHashError:
+                logger.error('got InventoryHashError')
+            else:
+                if oldest.inventory_hash != response['inventory_hash']:
+                    # Delete this holder from all it's resources to catch
+                    # later the ones it doesn't have anymore
+                    ResourceHolder.objects.filter(node__uuid=oldest.uuid).delete()
+                    try:
+                        remote_resources = remote_api.resource_list()
+                    except ResourceListError:
+                        logger.error('got ResourceListError')
+                    else:
+                        for remote_resource in remote_resources:
+                            uuid, timestamp=remote_resource['uuid'].split(TIMESTAMP_SEPARATOR)
+                            resource, created = NetworkResourceVersion.objects.get_or_create(
+                                uuid=uuid,
+                                timestamp=timestamp,
+                                defaults={
+                                    'metadata': dumps(remote_resource.get('metadata')),
+                                    'signature_properties': dumps(remote_resource.get('signature_properties')),
+                                }
+                            )
+                            resource.resourceholder_set.get_or_create(node=oldest)
 
+                        oldest.last_inventory_hash = datetime.datetime.now()
+                        oldest.inventory_hash = response['inventory_hash']
+                        oldest.save()
+                        # Delete network resources that have no holder
+                        NetworkResourceVersion.objects.filter(resourceholder=None).delete()
+            finally:
+                lock.release()
 
 def siblings_hash_check():
     '''
@@ -96,30 +108,39 @@ def siblings_hash_check():
         oldest = siblings[0]
         try:
             lock = Lock.acquire_lock(u''.join(['siblings_hash', oldest.uuid]), 20)
-            oldest.last_siblings_hash = datetime.datetime.now()
-            remote_api = RemoteCall(uuid=oldest.uuid)
-            response = remote_api.siblings_hash()
-            if oldest.siblings_hash != response['siblings_hash']:
-                for remote_sibling in remote_api.siblings_list():
-                    # Don't use a precomputed list, to a DB check each time
-                    # to minimize posibility of race cond
-                    # TODO: add locking
-                    try:
-                        Sibling.objects.get(uuid=remote_sibling['uuid'])
-                    except Sibling.DoesNotExist:
-                        # Only add nodes not known
-                        if remote_sibling['uuid'] != LocalNode.get().uuid:
-                            # Don't add self
-                            sibling = Sibling()
-                            sibling.uuid=remote_sibling['uuid']
-                            sibling.port=remote_sibling['port']
-                            sibling.ip_address=remote_sibling['ip_address']
-                            sibling.save()
-
-                oldest.siblings_hash = response['siblings_hash']
-                oldest.save()
-            lock.release()
         except LockError:
-            pass
-        except SiblingsHashError:
-            lock.release()
+            logging.debug('unable to acquire lock')
+        else:
+            remote_api = RemoteCall(uuid=oldest.uuid)
+            try:
+                response = remote_api.siblings_hash()
+            except SiblingsHashError:
+                logger.error('got SiblingsHashError')
+            else:
+                if oldest.siblings_hash != response['siblings_hash']:
+                    try:
+                        sibling_list = remote_api.siblings_list()
+                    except SiblingsListError:
+                        logger.error('got SiblingsListError')
+                    else:
+                        for remote_sibling in sibling_list:
+                            # Don't use a precomputed list, to a DB check each time
+                            # to minimize posibility of race cond
+                            # TODO: add locking
+                            try:
+                                Sibling.objects.get(uuid=remote_sibling['uuid'])
+                            except Sibling.DoesNotExist:
+                                # Only add nodes not known
+                                if remote_sibling['uuid'] != LocalNode.get().uuid:
+                                    # Don't add self
+                                    sibling = Sibling()
+                                    sibling.uuid=remote_sibling['uuid']
+                                    sibling.port=remote_sibling['port']
+                                    sibling.ip_address=remote_sibling['ip_address']
+                                    sibling.save()
+
+                        oldest.last_siblings_hash = datetime.datetime.now()
+                        oldest.siblings_hash = response['siblings_hash']
+                        oldest.save()
+            finally:
+                lock.release()
