eae02b29b72fda0a9f8ac70801bef3eb2b74a937
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-11 07:10:25 -0400

    Improve node key creation

--- apps/server_talk/__init__.py
+++ apps/server_talk/__init__.py
@@ -1,5 +1,4 @@
 import logging
-import socket
 
 from django.db.models.signals import post_syncdb
 from django.dispatch import receiver
@@ -13,34 +12,48 @@ from server_talk.conf.settings import (HEARTBEAT_QUERY_INTERVAL,
     INVENTORY_QUERY_INTERVAL, SIBLINGS_QUERY_INTERVAL)
 from server_talk.models import LocalNode
 from server_talk.api import NetworkCall
+from server_talk.literals import DEFAULT_PASSPHRASE
 
 logger = logging.getLogger(__name__)
 
 
 @receiver(post_syncdb, dispatch_uid='create_identify', sender=server_talk_model)
 def create_identify(sender, **kwargs):
-    node, created = LocalNode.objects.get_or_create(lock_id='1')
-    if created or not node.uuid or 1:
+    node = LocalNode()
+    if not node.uuid:
         print 'Creating local node identity ...'
         if kwargs['interactive']:
             name_real = raw_input('Enter a name to describe this node: ')
-            name_email = raw_input('Enter an e-mail associated with this node: ')
-            name_comment = raw_input('Enter comment for this node: ')
+            name_email = raw_input('Enter an e-mail associated with this node: ') or name_real
+            name_comment = raw_input('Enter comment for this node: ') or name_real
             while True:
-                passphrase = raw_input('Enter a passphrase to lock this key: ')
+                passphrase = raw_input('Enter a passphrase to lock this key (or just press Enter for none): ')
+                if not passphrase:
+                    passphrase_verify = passphrase
+                    break
                 passphrase_verify = raw_input('Enter again the same passphrase to verify: ')
                 if passphrase == passphrase_verify:
                     break
                 else:
                     print '\nPassphrases do not match, try again.\n'
+                if not passphrase:
+                    passphrase = None
         else:
-            name_real = socket.gethostbyname(socket.gethostname())
-            name_email = None
-            name_comment = None
+            name_real = 'Anonymous'
+            print 'Using %s as the node name' % name_real
+            name_email = name_real
+            name_comment = name_real
             passphrase = None
             # TODO: generate random passphrase
-            # TODO: save random passphrase in settings file
+            # TODO: save random passphrase in settings_local file
 
+        # TODO: fix, python-gpg is not signing data with non passphrase
+        # keys, assign a predicatable passphare as a default
+        # TODO: investigate the python-gpg source code
+        if not passphrase:
+            logger.debug('blank passphrase found, replacing with default')
+            passphrase = DEFAULT_PASSPHRASE
+            
         key_args = {
             'name_real': name_real,
             'name_email': name_email,
