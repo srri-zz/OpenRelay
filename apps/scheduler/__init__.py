fdc46303df13bad7fd9b4e1c4e415c7aeff0e810
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-02-13 10:58:30 -0400

    Fix scheduler variable name

--- apps/scheduler/__init__.py
+++ apps/scheduler/__init__.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_syncdb
 from django.dispatch import receiver
 
 from .api import register_interval_job, AlreadyScheduled
-from .runtime import scheduler
+from .runtime import backend_scheduler
 
 logger = logging.getLogger(__name__)
 
@@ -13,4 +13,4 @@ logger = logging.getLogger(__name__)
 @receiver(post_syncdb, dispatch_uid='scheduler_shutdown_post_syncdb')
 def scheduler_shutdown_post_syncdb(sender, **kwargs):
     logger.debug('Scheduler shut down on post syncdb signal')
-    scheduler.shutdown()
+    backend_scheduler.shutdown()
