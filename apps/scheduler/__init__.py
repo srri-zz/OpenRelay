639fbddc0ce839adebe699ad77f7a011b44d02ad
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-02-13 10:25:44 -0400

    Shutdown the scheduler during syncdb phase to avoid segmentation faults when creating the database

--- apps/scheduler/__init__.py
+++ apps/scheduler/__init__.py
@@ -1,1 +1,16 @@
-from scheduler.api import register_interval_job, AlreadyScheduled
+from __future__ import absolute_import
+import logging
+
+from django.db.models.signals import post_syncdb
+from django.dispatch import receiver
+
+from .api import register_interval_job, AlreadyScheduled
+from .runtime import scheduler
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_syncdb, dispatch_uid='scheduler_shutdown_post_syncdb')
+def scheduler_shutdown_post_syncdb(sender, **kwargs):
+    logger.debug('Scheduler shut down on post syncdb signal')
+    scheduler.shutdown()
