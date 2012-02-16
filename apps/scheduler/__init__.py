45b601bd720ddab43a4fbc808909e3d3c202e71b
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-02-16 01:51:53 -0400

    Use the signaler app to shutdown the scheduler during the collectstatic command

--- apps/scheduler/__init__.py
+++ apps/scheduler/__init__.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
+
 import logging
 
 from django.db.models.signals import post_syncdb
 from django.dispatch import receiver
+
+from signaler.signals import pre_collectstatic
 
 from .api import register_interval_job, AlreadyScheduled
 from .runtime import backend_scheduler
@@ -13,4 +16,10 @@ logger = logging.getLogger(__name__)
 @receiver(post_syncdb, dispatch_uid='scheduler_shutdown_post_syncdb')
 def scheduler_shutdown_post_syncdb(sender, **kwargs):
     logger.debug('Scheduler shut down on post syncdb signal')
+    backend_scheduler.shutdown()
+
+
+@receiver(pre_collectstatic, dispatch_uid='sheduler_shutdown_pre_collectstatic')
+def sheduler_shutdown_pre_collectstatic(sender, **kwargs):
+    logger.debug('Scheduler shut down on collectstatic signal')
     backend_scheduler.shutdown()
