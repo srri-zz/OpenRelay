bdd47699bdad1df3290258616fd29651053c9cb5
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-02-16 01:43:27 -0400

    Improve CompressedFile class to return instances of CompressedChild classes
    CompressedChild class add seeking support to the members of a compressed file as
    well as returning the entire path as the name of the member file

--- apps/openrelay_resources/compressed_file.py
+++ apps/openrelay_resources/compressed_file.py
@@ -1,10 +1,33 @@
 import zipfile
 
-from django.core.files.uploadedfile import SimpleUploadedFile
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from django.core.files import File
 
 
 class NotACompressedFile(Exception):
     pass
+
+
+class CompressedChild(File):
+    def __init__(self, file, name=None):
+        if name is None:
+            name = getattr(file, 'name', None)
+        self.name = name
+
+        try:
+            descriptor = file.open()
+        except AttributeError:
+            # Keep compatibility with other compressed file libraries
+            # that return a true file-like object and not an already
+            # opened descriptor like zfobj
+            descriptor = file
+
+        self.file = StringIO(descriptor.read())
+        descriptor.close()
 
 
 class CompressedFile(object):
@@ -16,7 +39,7 @@ class CompressedFile(object):
             # Try for a ZIP file
             zfobj = zipfile.ZipFile(self.file_object)
             filenames = [filename for filename in zfobj.namelist() if not filename.endswith('/')]
-            return (SimpleUploadedFile(name=filename, content=zfobj.read(filename)) for filename in filenames)
+            return (CompressedChild(file=zfobj.open(filename)) for filename in filenames)
         except zipfile.BadZipfile:
             raise NotACompressedFile
 
